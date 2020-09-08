e349acf48b75d8c131aa7e87a2597b4be8f67b98
panjigemilang <41560479+panjigemilang@users.noreply.github.com>
2020-09-08 23:00:41 +0700

    Updates

---
+++ precache-manifest.aef739e23de8b352ff8d73df360254c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f18282b9eb552d9ff52c1538aed8ec",
+    "url": "/Kulina/index.html"
+  },
+  {
+    "revision": "3b23d2dd41599c442db9",
+    "url": "/Kulina/static/css/main.d9e50f32.chunk.css"
+  },
+  {
+    "revision": "87212807e74a6914fedb",
+    "url": "/Kulina/static/js/2.c8d79b9c.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/Kulina/static/js/2.c8d79b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b23d2dd41599c442db9",
+    "url": "/Kulina/static/js/main.ad0436ea.chunk.js"
+  },
+  {
+    "revision": "fbb9f36240c529d7bacf",
+    "url": "/Kulina/static/js/runtime-main.bca86162.js"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/Kulina/static/media/Product-Sans-Regular.eae9c18c.ttf"
+  },
+  {
+    "revision": "4a8b12bc16d03d1b28a75ee2610f6411",
+    "url": "/Kulina/static/media/product_sans_regular-webfont.4a8b12bc.woff"
+  },
+  {
+    "revision": "903cc7f4dec5a4fd9bdd9639dfd7cf49",
+    "url": "/Kulina/static/media/product_sans_regular-webfont.903cc7f4.woff2"
+  },
+  {
+    "revision": "7f4b330e4a9c05c60742a2a05260477f",
+    "url": "/Kulina/static/media/ucing.7f4b330e.jpg"
+  }
+]);
